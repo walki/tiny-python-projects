19438ce68eb322b4aeb9820d13f540396658907e
Roger Walker <roger.john.walker@gmail.com>
2022-04-22 22:04:57 -0400

    Chapter 1 - maybe not the best book, don't love the idea of a template to start when you are learning

--- 01_hello/hello_pytest.py
+++ 01_hello/hello_pytest.py
@@ -1,8 +1,12 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 """tests for hello.py"""
 
+from cgitb import text
+import sys
+import subprocess
 import os
-from subprocess import getstatusoutput, getoutput
+from subprocess import run
+
 
 prg = './hello.py'
 
@@ -14,5 +18,19 @@ def test_exists():
 def test_runnable():
     """runs using python"""
 
-    out = getoutput(f'python {prg}')
-    assert out.strip() == 'Hello, World!'
+    result = subprocess.run([sys.executable, prg], capture_output=True, text=True)
+    assert result.stdout.strip() == 'Hello, World!'
+
+def test_usage():
+    """usage"""
+
+    for flag in ['-h', '--help']:
+        result = subprocess.run([sys.executable, prg, flag], check=True, capture_output=True, text=True)
+        assert result.returncode == 0
+        assert result.stdout.lower().startswith('usage')
+
+def test_name():
+    for flag in ['-n', '--name']:
+        result = subprocess.run([sys.executable, prg, flag, 'Testing'], check=True, capture_output=True, text=True)
+        assert result.returncode == 0
+        assert result.stdout.strip() == 'Hello, Testing!'
